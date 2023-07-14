74142711bf303a5422b8be4950dd4fa593c6ae0d
Arkitu <arkitu@users.noreply.github.com>
2023-07-14 16:48:57 +0200

    noice

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use std::env;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 use async_recursion::async_recursion;
+use fixedstr::str256;
 
-type Memory<'a> = HashMap<String, Value<'a>>;
+type VariableName<'a> = str256;
+type Memory<'a> = HashMap<VariableName<'a>, Value<'a>>;
 
 #[derive(Clone)]
 struct Scope<'a> {
@@ -19,11 +21,11 @@ impl<'a> Scope<'a> {
             parent
         }
     }
-    fn insert(&mut self, name: String, value: Value<'a>) {
+    fn insert(&mut self, name: VariableName, value: Value<'a>) {
         self.memory.insert(name, value);
     }
     #[async_recursion]
-    async fn get(&self, name: String) -> Option<Value<'a>> {
+    async fn get(&self, name: VariableName) -> Option<Value<'a>> {
         match self.memory.get(&name) {
             Some(value) => Some(value.clone()),
             None => {
@@ -46,7 +48,7 @@ enum Number {
 
 #[derive(Clone, Copy)]
 struct Function<'a> {
-    args: &'a Vec<Arc<String>>,
+    args: &'a Vec<VariableName<'a>>,
     body: &'a Vec<Expression<'a>>
 }
 
@@ -59,18 +61,18 @@ enum Value<'a> {
     None,
     Undefined,
     Function(Arc<Function<'a>>),
-    Object(Arc<HashMap<String, Value<'a>>>),
+    Object(Arc<Memory<'a>>),
     Array(Arc<Vec<Value<'a>>>)
 }
 
 #[derive(Clone)]
 enum Expression<'a> {
     // variable name, value
-    Assignement(String, Value<'a>),
+    Assignement(VariableName<'a>, Arc<Expression<'a>>),
     // function, args
-    FunctionCall(Arc<Function<'a>>, Vec<Value<'a>>),
+    FunctionCall(Arc<Function<'a>>, Vec<Expression<'a>>),
     // variable name
-    Variable(String),
+    Variable(VariableName<'a>),
     // value
     Value(Value<'a>),
     None
@@ -79,15 +81,15 @@ impl<'a> Expression<'a> {
     #[async_recursion]
     async fn run(&self, scope: Arc<RwLock<Scope<'a>>>) -> Value<'a> {
         match self {
-            Expression::Assignement(name, value) => {
-                scope.write().await.insert(name.clone(), value.clone());
+            Expression::Assignement(name, exp) => {
+                scope.write().await.insert(name.clone(), exp.run(scope.clone()).await.clone());
                 Value::None
             },
             Expression::FunctionCall(function, args) => {
-                let mut fn_scope = Scope::new(Some(scope));
+                let mut fn_scope = Scope::new(Some(scope.clone()));
                 let function = *function.clone();
-                for (arg, value) in function.args.iter().zip(args) {
-                    fn_scope.insert(arg.to_string(), value.clone());
+                for (arg, exp) in function.args.iter().zip(args) {
+                    fn_scope.insert(*arg, exp.run(scope.clone()).await.clone());
                 }
                 let mut result = Value::None;
                 let fn_scope = Arc::new(RwLock::new(fn_scope));
@@ -108,16 +110,44 @@ impl<'a> Expression<'a> {
     }
 }
 
-fn main() {
+fn parse_expression(exp: &str) -> Expression<'static> {
+    let exp = exp.trim();
+
+    if exp.len() == 0 {
+        return Expression::None
+    }
+
+    let mut tokens: Vec<&str> = exp.split(' ').collect();
+
+    if tokens.contains(&"=") {
+        if tokens[0] == "var" || tokens[0] == "let" || tokens[0] == "const" {
+            tokens.remove(0);
+        }
+
+        let var_name = tokens[0];
+
+        let value = &tokens[tokens.iter().position(|&t| t == "=").unwrap()+1..];
+        let value = parse_expression(&value.join(" "));
+
+        Expression::Assignement(var_name.into(), Arc::new(value))
+    } else {
+        Expression::None
+    }
+}
+
+#[tokio::main]
+async fn main() {
     let args = env::args().collect::<Vec<String>>();
     let filename = &args[1];
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    let lines = contents.split(';');
-
-    let mut memory: HashMap<String, Value> = HashMap::new();
+    let expressions = contents.split(';');
 
     let mut root_scope = Scope::new(None);
+
+    for exp in expressions {
+        parse_expression(exp);
+    }
 }
